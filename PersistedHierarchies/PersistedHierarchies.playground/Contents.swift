5016f3f7769044048b30b690c4d56d3cafe7e6f3
Andy Dent <dent@oofile.com.au>
2018-02-22 02:07:52 +0800

    Non-functional version of PersistedHierarchies, before rearrange how uses JSONEncoder

--- PersistedHierarchies/PersistedHierarchies.playground/Contents.swift
+++ PersistedHierarchies/PersistedHierarchies.playground/Contents.swift
@@ -1,14 +1,117 @@
 //: PersistedHierarchies - use class hierarchy which is not trivially Codable
 //: See the Codable.Hierarchies folder for an explanation and an alternative
-//: **Assume you have to use inheritance**
+//: **Assume you have to use inheritance** and want to put those types into a heterogenous array of the base type
 
 import Cocoa
 
+// Generic reusable stuff you just need once
+protocol HierDecoder {
+  func read() -> String
+  func read() -> Int
+  func read() -> Bool
+  func read<T>() -> [T]
+}
 
-class BaseBeast {
+protocol HierEncoder {
+  func write(_:String)
+  func write(_:Int)
+  func write(_:Bool)
+  func write(_:HierCodable)
+  func write(_:[HierCodable])
+  func pushContext()
+  func popContext()
+}
+
+// protocol so generic used in EncoderUsing can comply
+protocol UnkeyedContainerSupplier {
+  func unkeyedContainer() -> UnkeyedEncodingContainer
+}
+extension JSONEncoder : UnkeyedContainerSupplier {}
+
+class EncoderUsing<T:UnkeyedContainerSupplier>: HierEncoder {
+  let realEncoder:T
+  var containerStack = [UnkeyedEncodingContainer]()
+  lazy var container: UnkeyedEncodingContainer? = nil
+  
+  init(_ enc:T) {
+    realEncoder = enc
+    pushContext()  // start with a top level in which we write all the objects
+  }
+  
+  func pushContext()
+  {
+    if container != nil {
+      containerStack.append(container!)  // new container for each start, typically a chain of them
+    }
+    container = realEncoder.unkeyedContainer()
+  }
+  
+  func popContext()
+  {
+    container = containerStack.popLast()
+  }
+
+  func write(_ value:String) {
+    try! container?.encode(value)
+  }
+  func write(_ value:Int) {
+    try! container?.encode(value)
+  }
+  func write(_ value:Bool) {
+    try! container?.encode(value)
+  }
+}
+
+// default implementations so all collections of HierCodable can just be written
+extension HierEncoder {
+  func write(_ typedObject:HierCodable) {
+    write(typedObject.type())
+    typedObject.encode(to: self)
+  }
+  func write(_ typedObjects:[HierCodable]) {
+    // nested collections should start a new container
+    pushContext()
+    typedObjects.forEach {  // same as $0.write()
+      write($0.type())
+      $0.encode(to: self)
+    }
+    popContext()
+  }
+}
+
+protocol HierCodable {
+  func type() -> Int
+  func encode(to:HierEncoder)
+}
+
+typealias DecoderFactory = (inout HierDecoder) throws -> HierCodable
+
+// one point to register and maintain list of factories
+class HierCodableFactories {
+  private static var Factories = [DecoderFactory]()
+  static func Register(decodesType:Int, from factory:@escaping DecoderFactory) -> Int
+  {
+    Factories[decodesType] = factory
+    return decodesType
+  }
+
+}
+
+//:---- Example classes using HierCodable
+
+class BaseBeast : HierCodable {
   let name: String
   init(name:String) { self.name = name }
   func move() -> String { return "\(name) Sits there looking stupid" }
+  
+  //MARK HierCodable
+  private static let typeCode:Int = HierCodableFactories.Register(decodesType:0) {
+    (from) in return BaseBeast(name:from.read())
+  }
+  func type() -> Int { return BaseBeast.typeCode }
+  func encode(to:HierEncoder) {
+    to.write(name)
+  }
 }
 
 class Flyer : BaseBeast {
@@ -18,6 +121,16 @@ class Flyer : BaseBeast {
     super.init(name: name)
   }
   override func move() -> String { return "\(name)  Flies up to \(maxAltitude)"}
+
+  //MARK HierCodable
+  private static let typeCode = HierCodableFactories.Register(decodesType:1) {
+    (from) in return Flyer(name:from.read(), maxAltitude:from.read())
+  }
+  override func type() -> Int { return Flyer.typeCode }
+  override func encode(to:HierEncoder) {
+    super.encode(to:to)
+    to.write(maxAltitude)
+  }
 }
 
 class Walker : BaseBeast {
@@ -35,15 +148,38 @@ class Walker : BaseBeast {
     let maybeWaggle = hasTail ? "waggling its tail" : ""
     return "\(name) Runs on \(numLegs) legs \(maybeWaggle)"
   }
+
+  //MARK HierCodable
+  private static let typeCode = HierCodableFactories.Register(decodesType:1) {
+    (from) in return Walker(name:from.read(), legs:from.read(), hasTail:from.read())
+  }
+  override func type() -> Int { return Walker.typeCode }
+  override func encode(to:HierEncoder) {
+    super.encode(to:to)
+    to.write(numLegs)
+    to.write(hasTail)
+  }
 }
 
-struct Zoo {
+struct Zoo : HierCodable {
   var creatures = [BaseBeast]()
   func dump() {
     creatures.forEach { print($0.move()) }
   }
+  
+  //MARK HierCodable
+  private static let typeCode:Int = HierCodableFactories.Register(decodesType:4) {
+    (from) in return Zoo(creatures:from.read())
+  }
+  func type() -> Int { return Zoo.typeCode }
+  func encode(to:HierEncoder) {
+    // write zoo typecode here????????
+    to.write(creatures)
+  }
 }
 
+
+//: ---- Demo of encoding and decoding working ----
 let startZoo = Zoo(creatures: [
   Flyer(name:"Kookaburra", maxAltitude:5000),
   BaseBeast(name:"Rock"),
@@ -53,3 +189,12 @@ let startZoo = Zoo(creatures: [
   ])
 
 startZoo.dump()
+
+print("---------\ntesting JSON\n")
+let encoder = JSONEncoder()
+encoder.outputFormatting = .prettyPrinted
+let hierEnc = EncoderUsing(encoder)
+startZoo.encode(to:hierEnc)
+//let encData = try encoder.encode(startZoo)
+//print(String(data:encData, encoding:.utf8)!)
+
