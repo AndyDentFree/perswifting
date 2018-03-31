a2276a67f0607f0abe61d99fcead2e4351fa7723
Andy Dent <dent@oofile.com.au>
2018-03-31 11:19:50 +0800

    Backported bugfix for empty arrays and internally renamed typedObject to typedThing to make clear it works for structs and enums too

--- PersistedHierarchies/RicherPersistedHierarchies.playground/Sources/HierCodable.swift
+++ PersistedHierarchies/RicherPersistedHierarchies.playground/Sources/HierCodable.swift
@@ -39,29 +39,29 @@ public protocol HierDecoder {
 
 // default implementations so all collections of HierCodable can just be written
 extension HierEncoder {
-  public func write(_ typedObject:HierCodable) {
-    write(typedObject.typeKey())  // for decoding, code precedes nested context
+  public func write(_ typedThing:HierCodable) {
+    write(typedThing.typeKey())  // for decoding, code precedes nested context
     pushContext()
-    typedObject.encode(to: self)
+    typedThing.encode(to: self)
     popContext()
   }
   
   // unlike optional primitives, for objects we use a default empty typecode
   // individual binary encoders are allowed to implement how they write optional primitives
-  public func write(_ typedObject:HierCodable?) {
-    if typedObject == nil {
+  public func write(_ typedThing:HierCodable?) {
+    if typedThing == nil {
       write("")
     }
     else {
-      write(typedObject!)
+      write(typedThing!)
     }
   }
   
-  public func write(_ typedObjects:[HierCodable]) {
+  public func write(_ typedThings:[HierCodable]) {
     // nested collections start a new container
     pushContext()
-    write(typedObjects.count)  // leading count in default format
-    typedObjects.forEach {
+    write(typedThings.count)  // leading count in default format
+    typedThings.forEach {
       write($0)
     }
     popContext()
@@ -74,10 +74,9 @@ extension HierDecoder {
     if let key:String = try? read() {
       pushContext()
       if let factory = HierCodableFactories.factory(key:key) {
-        if var ret: HierCodable = try factory(self) {
-          popContext()
-          return ret
-        }
+        let ret: HierCodable = try factory(self)
+        popContext()
+        return ret
       }
       throw DecodeError.noObjectBodyForKey(key:key)
     }
@@ -87,14 +86,13 @@ extension HierDecoder {
   public func readOptionalObject() throws -> HierCodable?  {
     if let key:String = try? read() {
       if key.count == 0 {
-        return nil  // validly detected a None optional
+        return nil  // validly detected a None optional indicated by blank key
       }
       pushContext()
       if let factory = HierCodableFactories.factory(key:key) {
-        if var ret: HierCodable = try factory(self) {
-          popContext()
-          return ret
-        }
+        let ret: HierCodable = try factory(self)
+        popContext()
+        return ret
       }
       throw DecodeError.noObjectBodyForKey(key:key)
     }
@@ -108,9 +106,15 @@ extension HierDecoder {
     pushContext()
     var ret = [HierCodable]()
     if let numToDecode:Int = try? read() {  // match default write which preceds with length
-      for _ in 1...numToDecode  { // typecode and nested container for each
-        if let obj = try? readObject() {
-          ret.append(obj)
+      if numToDecode == 0 {
+        // print("Empty array but that's OK")
+        // Note for some reason the loop below caused an error - cannot create a range
+      }
+      else {
+        for _ in 1...numToDecode  { // typecode and nested container for each
+          if let obj = try? readObject() {
+            ret.append(obj)
+          }
         }
       }
     }
